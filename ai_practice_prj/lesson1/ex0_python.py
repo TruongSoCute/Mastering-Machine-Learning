03cb0996b6a8110fd39afaae855689a2ac3753da
TruongSoCute <qtruong.hs@gmail.com>
2025-08-10 14:41:00 +0700

    upload first day learning ML

---
+++ ai_practice_prj/lesson1/ex0_python.py
@@ -0,0 +1,1 @@
+print("Hello, Im from external file")
---
+++ ai_practice_prj/lesson1/ex1_calc.py
@@ -0,0 +1,5 @@
+print ("Calucator Basic")
+a = int(input("Enter Value A:"))
+b = int(input("Enter Value B:"))
+c = a + b
+print("Value Of C = %d + %d = %d" % (a,b,c))
